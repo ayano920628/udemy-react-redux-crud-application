461b0ebb973dda251695cdefa71d6734ac0dc51d
ayano <ayano920628@gmail.com>
2019-11-23 22:04:31 +0900

    create component and function  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { directive } from '@babel/types';
+// import { directive } from '@babel/types';
 // import React, { Component } from 'react';
 
-function App() {
+const App = () => {
   return (
     <React.Fragment>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("hello")}} />
+      <Cat />
     </React.Fragment>
   );
 }
+
+const Cat = () => {
+  return (
+    <div>meaw!</div>
+  );
+}
+
 
 export default App;
