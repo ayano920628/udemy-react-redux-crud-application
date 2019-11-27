13f2b41c2bfa1815cdafe8d00e2bb35bc367f0b1
ayano <ayano920628@gmail.com>
2019-11-27 20:59:00 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,29 @@
 // import PropTypes from 'prop-types';
 // import { directive } from '@babel/types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
+// const App = () => (<Counter></Counter>)
 
-const App = () => (<Counter></Counter>)
-
-class Counter extends Component {
-  constructor(props){
-    super(props);
-    this.state = {count: 0}
-  }
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
+class App  extends Component {
+  // constructor(props){
+  //   super(props);
+    // this.state = {count: 0}
+  // }
+  // handlePlusButton = () => {
+  //   this.setState({ count: this.state.count + 1 })
+  // }
+  // handleMinusButton = () => {
+  //   this.setState({ count: this.state.count - 1 })
+  // }
 
   render() {
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     );
   }
@@ -60,4 +62,13 @@ class Counter extends Component {
 //   age : PropTypes.number.isRequired
 // }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({ increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// export default App;
