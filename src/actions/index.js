355ebf9cc472f49ce46d571230077e29518092e0
ayano <ayano920628@gmail.com>
2019-11-30 10:18:24 +0900

    inplement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // export const INCREMENT = 'INCREMENT';
 // export const DECREMENT = 'DECREMENT';
 export const READ_EVENTS = 'READ_EVENTS';
+export const CREATE_EVENT = 'CREATE_EVENT';
 
 // export const increment = () => ({
 //   type: INCREMENT
@@ -18,4 +19,9 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENT, response })
 }
